01d920dcf6ab34f75dbab77faf77d05acdb407b2
Dmitry Drobotov <dmitry.drobotov123@gmail.com>
2021-06-03 08:35:38 +0300

    Fix images order issues

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,7 +3,11 @@ class Image < ApplicationRecord
   has_many :values
 
   # get images array of arrays by given theme_id
-  scope :theme_images, ->(theme_id) { select('id', 'name', 'file', 'avg_value').where(theme_id: theme_id) }
+  scope :theme_images, lambda { |theme_id|
+                         select('id', 'name', 'file', 'avg_value')
+                           .where(theme_id: theme_id)
+                           .order('id')
+                       }
 
   scope :find_image, ->(image_id) { find(image_id) }
 
